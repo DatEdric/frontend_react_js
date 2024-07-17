49d6c0fb88693187c1a2f5474165a07ba3cc9cf9
Tandat <buidat027@gamil.com>
2024-07-17 15:30:54 +0700

    connect react to express server

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('/api')
+      .then(response => response.json())
+      .then(data => setMessage(data.message));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>{message}</h1>
+    </div>
+  );
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5000, // Đổi 3000 thành số cổng bạn muốn
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+  }
+})
